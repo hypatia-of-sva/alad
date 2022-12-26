2ad63827d9705ba76219c0915d9355ca7cf37176
hypatia-of-sva <121448956+hypatia-of-sva@users.noreply.github.com>
2022-12-26 17:49:42 +0100

    Update alad.h

--- alad.h
+++ alad.h
@@ -23,6 +23,7 @@
  *  Unload the library loaded with default initalization with
  * 
  *          aladTerminate();
+ *
  *  This will not unload the function pointers or set them to NULL, but they will be probably invalid after unloading the shared library.
  * 
  *  
